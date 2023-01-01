c5c5edb4113ff396d46107b71ad360e7a333c431
Basar Buyukkahraman <basarbk@yahoo.com>
2023-01-01 13:59:19 +0300

    change port

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -40,7 +40,7 @@ beforeAll(async () => {
   await sequelize.sync();
 
   await new Promise((resolve) =>
-    server.listen(8587, 'localhost', (params) => {
+    server.listen(18587, 'localhost', (params) => {
       console.log('server initialized', params)
       resolve();
     })
